9025062f1a84e17888cacb6993f7cf6b2e5eeeb6
nemo <luoxincheng@didichuxing.com>
2018-05-16 15:27:22 +0800

    Save by Nemo ( luoxincheng )

--- template/newpage.sh
+++ template/newpage.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo $1
 if  [ ! -n "$1" ] ;then
     echo "输入页面名"
     exit 0
@@ -8,7 +7,6 @@ shellpath=$(dirname $0)
 path=${shellpath%/template}
 compName=$1
 CompName="$(tr '[:lower:]' '[:upper:]' <<< ${compName:0:1})${compName:1}"
-echo $CompName
 cp $path/template/template.js $path/src/redux/$compName.js
 sed -i "" "s/template/$compName/g" $path/src/redux/$compName.js
 mkdir $path/src/page/$CompName
