abb7b24b38c68a6d527dcefd1fa913009b9ecb95
nemo <luoxincheng@didichuxing.com>
2018-05-16 21:27:25 +0800

    Save by Nemo ( luoxincheng )

--- template/newpage.sh
+++ template/newpage.sh
@@ -7,8 +7,8 @@ shellpath=$(dirname $0)
 path=${shellpath%/template}
 compName=$1
 CompName="$(tr '[:lower:]' '[:upper:]' <<< ${compName:0:1})${compName:1}"
-cp $path/template/template.js $path/src/redux/$compName.js
-sed -i "" "s/template/$compName/g" $path/src/redux/$compName.js
+cp $path/template/template.js $path/src/store/$compName.js
+sed -i "" "s/template/$compName/g" $path/src/store/$compName.js
 mkdir $path/src/page/$CompName
 cp $path/template/template.jsx $path/src/page/$CompName/index.jsx
 sed -i "" "s/template/$compName/g" $path/src/page/$CompName/index.jsx
