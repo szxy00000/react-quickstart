e7787ddd44fd29525ff9b54d0a154549e4e03411
nemo <luoxincheng@didichuxing.com>
2018-07-03 12:37:44 +0800

    save

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,6 @@
-const protocol = 'http:';
-const severUrl = '//10.84.144.229';
-const port = ':8051/';
-const catalog = 'api/';
-const url = `${protocol}${severUrl}${port}${catalog}`;
-const taskUrl = `${url}tasks/`;
-const taskResultsUrl = `${url}task_results/`;
-const productsUrl = `${url}products/`;
-const workersUrl = `${url}workers/`;
+const protocol = 'http';
 
 export {
-    taskUrl,
-    taskResultsUrl,
-    productsUrl,
-    workersUrl
+    
 }
+
