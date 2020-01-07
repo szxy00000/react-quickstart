36c434277b3d597c74b4dedfc87b3e5ff4370d01
nemo <luoxincheng@didiglobal.com>
2020-01-07 15:26:48 +0800

    save

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,8 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const PrepackWebpackPlugin = require('prepack-webpack-plugin').default;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// const Dashboard = require('webpack-dashboard');
-// const DashboardPlugin = require('webpack-dashboard/plugin');
-// const dashboard = new Dashboard();
-
-const ROOT_PATH = path.resolve(__dirname);
-const NPM_PATH = path.resolve(ROOT_PATH, 'node_modules');
 //prepack配置
 const configuration = {
   mathRandomSeed: 0
@@ -18,104 +10,95 @@ const configuration = {
 
 
 module.exports = {
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    contentBase : path.resolve(__dirname , 'template'),
-    publicPath : '/',
-    host : '0.0.0.0',
-    port : 8089
+    contentBase: path.resolve(__dirname, "template"),
+    publicPath: "/",
+    host: "0.0.0.0",
+    port: 8089
   },
-  entry: './index.jsx',
+  entry: ["babel-polyfill", "./index.jsx"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js"
   },
-  context: path.resolve(__dirname, 'src'),
+  context: path.resolve(__dirname, "src"),
   resolve: {
-    extensions: ['.js', '.json', '.jsx'],
+    extensions: [".js", ".json", ".jsx"],
     alias: {
-      common: path.resolve(__dirname, 'src/common'),
-      component: path.resolve(__dirname, 'src/component'),
-      config: path.resolve(__dirname, 'src/config'),
-      page: path.resolve(__dirname, 'src/page'),
-      static: path.resolve(__dirname, 'src/static'),
-      store: path.resolve(__dirname, 'src/store'),
-      pure: path.resolve(__dirname, 'src/pure')
+      common: path.resolve(__dirname, "src/common"),
+      component: path.resolve(__dirname, "src/component"),
+      config: path.resolve(__dirname, "src/config"),
+      page: path.resolve(__dirname, "src/page"),
+      static: path.resolve(__dirname, "src/static"),
+      store: path.resolve(__dirname, "src/store"),
+      pure: path.resolve(__dirname, "src/pure")
     }
   },
   externals: {
-    'AMap' : 'AMap',
-    'AMapUI': 'AMapUI'
+    AMap: "AMap",
+    AMapUI: "AMapUI"
   },
   module: {
     rules: [
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.(js|jsx)$/,
-        exclude: path.resolve(__dirname, 'node_modules'),
-        loader : 'eslint-loader',
+        exclude: path.resolve(__dirname, "node_modules"),
+        loader: "eslint-loader",
         options: {
           emitWarning: true
         }
       },
       {
         test: /\.(js|jsx)$/,
-        exclude: path.resolve(__dirname, 'node_modules'),
-        use: 'babel-loader'
-      }, {
+        exclude: path.resolve(__dirname, "node_modules"),
+        use: "babel-loader"
+      },
+      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }, {
+        use: ["style-loader", "css-loader"]
+      },
+      {
         test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader']
-      }, {
+        use: ["style-loader", "css-loader", "less-loader"]
+      },
+      {
         test: /\.(jpg|png|gif|ttf|otf)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192,
-            name: 'img/[name].[ext]'
-          },
-        }]
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "img/[name].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
     //new PrepackWebpackPlugin(configuration),
     new webpack.DefinePlugin({
-      IS_PRODUCTION: JSON.stringify(false),
-    }),
-    // react的公共模块
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor'],
-      filename: 'vendor.js',
-      minChunks: ({resource}) => {
-        resource &&
-          resource.indexOf('node_modules') &&
-          resource.match(/\.js$/);
-      }
+      IS_PRODUCTION: JSON.stringify(false)
     }),
 
     // enable HMR globally
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      'setConfig' : path.resolve(
-        __dirname,
-        './src/config/devQueueConfig'
-      ),
+      $: "jquery",
+      setConfig: path.resolve(__dirname, "./src/config/devQueueConfig")
     }),
 
     // 使用webpack-dashboard，但是输出的信息在item2里，显示容易错乱
     // new DashboardPlugin(dashboard.setData)
 
-
     new HtmlWebpackPlugin({
-      title: 'nemo',
-      filename: 'index.html',
+      title: "nemo",
+      filename: "index.html",
       // 关闭该插件默认的注入css、js，完全由模版控制
       inject: false,
-      template: '../template/index.html',
-    }),
+      template: "../template/index.html"
+    })
   ]
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,8 +3,6 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const ROOT_PATH = path.resolve(__dirname);
@@ -12,97 +10,80 @@ const NPM_PATH = path.resolve(ROOT_PATH, 'node_modules');
 
 module.exports = {
   entry: {
-    'duse-eye-app': './index.jsx'
+    "react-app": ["babel-polyfill", "./index.jsx"]
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle-[chunkhash].js'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle-[chunkhash].js"
   },
-  context: path.resolve(__dirname, 'src'),
+  context: path.resolve(__dirname, "src"),
   resolve: {
-    extensions: ['.js', '.json', '.jsx'],
+    extensions: [".js", ".json", ".jsx"],
     alias: {
-      common: path.resolve(__dirname, 'src/common'),
-      component: path.resolve(__dirname, 'src/component'),
-      config: path.resolve(__dirname, 'src/config'),
-      page: path.resolve(__dirname, 'src/page'),
-      static: path.resolve(__dirname, 'src/static'),
-      store: path.resolve(__dirname, 'src/store'),
-      pure: path.resolve(__dirname, 'src/pure')
+      common: path.resolve(__dirname, "src/common"),
+      component: path.resolve(__dirname, "src/component"),
+      config: path.resolve(__dirname, "src/config"),
+      page: path.resolve(__dirname, "src/page"),
+      static: path.resolve(__dirname, "src/static"),
+      store: path.resolve(__dirname, "src/store"),
+      pure: path.resolve(__dirname, "src/pure")
     }
   },
   externals: {
-    'AMap' : 'AMap',
-    'AMapUI': 'AMapUI',
+    AMap: "AMap",
+    AMapUI: "AMapUI"
   },
   module: {
     rules: [
       {
+        enforce: "pre",
         test: /\.(js|jsx)$/,
-        exclude: path.resolve(__dirname, 'node_modules'),
-        use: 'babel-loader'
-      }, {
+        exclude: path.resolve(__dirname, "node_modules"),
+        loader: "eslint-loader",
+        options: {
+          emitWarning: true
+        }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: path.resolve(__dirname, "node_modules"),
+        use: "babel-loader"
+      },
+      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }, {
+        use: ["style-loader", "css-loader"]
+      },
+      {
         test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader']
-      }, {
+        use: ["style-loader", "css-loader", "less-loader"]
+      },
+      {
         test: /\.(jpg|png|gif|ttf|otf)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192,
-            name: 'static/img/[name].[ext]',
-          },
-        }]
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "img/[name].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      IS_PRODUCTION: JSON.stringify(true),
+      IS_PRODUCTION: JSON.stringify(true)
     }),
-
-    // react的公共模块
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor'],
-      filename: 'vendor.js',
-      minChunks: ({resource}) => {
-          resource &&
-          resource.indexOf('node_modules') &&
-          resource.match(/\.js$/)
-      }
-    }),
-
-    // 每个文件里默认引入jquery
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      setConfig : path.resolve(
-        __dirname,
-        "./src/config/proQueueConfig"
-      ),
-    }),
-
-    // 压缩输出的js
-    new webpack.optimize.UglifyJsPlugin({
-      // mangle: {
-      //   toplevel: true
-      // },
-      // compress: {
-      //   passes: 4
-      // }
-    }),
-
     // 生成最后的html文件（渲染模版的方式）
     // 这样可以带过去文件的hash值，也方便迭代管理
     new HtmlWebpackPlugin({
-      title: 'NEMO',
-      filename: 'index.html',
+      title: "NEMO",
+      filename: "index.html",
       // 关闭该插件默认的注入css、js，完全由模版控制
       inject: false,
-      template: '../template/index.html',
-    }),
+      template: "../template/index.html"
+    })
   ]
 };
